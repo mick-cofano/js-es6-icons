a94a55ad503d94dc2f1677e45469260f8ed4e66b
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-21 23:25:13 +0100

    Inizio esercizio...

---
+++ js/scrript.js
@@ -0,0 +1,5 @@
+// Milestone 1:
+// Definire un array di oggetti; ogni oggetto rappresenta un'icona,
+// che è caratterizzata da: nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template literal,
+// visualizzare in pagina tutte le icone con il proprio nome.
